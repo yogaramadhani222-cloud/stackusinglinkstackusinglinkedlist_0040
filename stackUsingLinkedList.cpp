cb21acc2fb6be2c894cc957d7001bb22efecc06e
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-14 16:18:00 +0700

    menambahkan switch case dan exit program

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -86,6 +86,14 @@ int main()
         cout << "4.Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
-    }
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        }
     return 0;
 }
